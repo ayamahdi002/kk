efa07db6543ee1ba29f2e0cecba9a4202255a917
ayamahdi002 <132265676+ayamahdi002@users.noreply.github.com>
2023-05-20 08:30:28 -1100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Doc\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Doc\\flutter-app\\happy_tails_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Doc\\flutter"
+  "PROJECT_DIR=C:\\Doc\\flutter-app\\happy_tails_app"
+  "FLUTTER_ROOT=C:\\Doc\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Doc\\flutter-app\\happy_tails_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Doc\\flutter-app\\happy_tails_app"
+  "FLUTTER_TARGET=C:\\Doc\\flutter-app\\happy_tails_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Doc\\flutter-app\\happy_tails_app\\.dart_tool\\package_config.json"
+)
